097e174dda7f46eff68e0169511b77938e8f65b5
jaya <jayakrushnamahapatro8@gmail.com>
2023-05-16 05:24:43 +0530

    SpringBoot

---
+++ BookStore/src/main/java/com/BookStore/BookStore/repository/MyBookRepository.java
@@ -0,0 +1,13 @@
+package com.BookStore.BookStore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.BookStore.BookStore.entity.MyBookList;
+
+
+
+@Repository
+public interface MyBookRepository extends JpaRepository<MyBookList,Integer>{
+
+}
